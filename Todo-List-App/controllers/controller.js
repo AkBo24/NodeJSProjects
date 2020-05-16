420f53c9b784ceb249c99cc4ed221c0ef19f3d78
AkBo24 <akshaybodla21@gmail.com>
2020-05-16 15:44:23 -0700

    Fixed rendering from mongo db, and created adding to db functionality

--- Todo-List-App/controllers/controller.js
+++ Todo-List-App/controllers/controller.js
@@ -12,26 +12,41 @@ mongoose.connect('mongodb+srv://todoUser:admin@todo-app-practice-hcsru.mongodb.n
 });
 
 //Set up schema and model for mongodb (Schema - a template)
-const mongooseSchema = new mongoose.Schema({
+const todoSchema = new mongoose.Schema({
   item: String
 });
-const Todo = mongoose.model('Todo', mongooseSchema);
+const Todo = mongoose.model('Todo', todoSchema);
 
 //Dummy data
-const item = Todo({item: 'Finish project'}).save((err) => {
-  if(err) throw err;
-})
+// const item = Todo({item: 'Finish project'}).save((err) => {
+//   if(err) throw err;
+// })
 
-let   data = [{item: "todo 1"}, {item: "todo 2"}, {item: "todo 3"}]; //dummy data
+let   data = [{item: "todo 1"}, {item: "todo 2"}, {item: "todo 3"}];
+
 
 routes = (app) => {
   app.get('/todo', (req, res) => {
-    res.render('todo.ejs', {data: data});
+    //Connect to the database and push each item inside the todo collection to the
+    //data array
+    Todo.find({}, (err, docs) => {
+      if(err) throw err;
+      res.render('todo.ejs', {data: docs})
+    })
+
+    // res.render('todo.ejs', {data:data})
   });
 
   app.post('/todo', urlencodedParser, (req, res) => {
-    data.push( req.body );
-    res.render('todo.ejs', {data: data});
+
+    //adding new item by connecting to database
+    const newTodo = Todo(req.body).save( (err) => {
+      if(err) throw err;
+      res.render('todo.ejs', {data: data});
+    });
+
+    // data.push( req.body );
+    // res.render('todo.ejs', {data: data});
     // res.json(data);
   });
 
